c981c912af2ac0acd049a5b2a9a5c3532767c011
LinelinLove <chrisline.lin@outlook.fr>
2024-11-22 13:57:52 +0100

    --add error handler and temperature for rag

--- rag.py
+++ rag.py
@@ -18,11 +18,24 @@ def main():
     api_key = os.getenv("CLOUD_API_KEY")
     file_path = './pdf/temp.pdf'
 
-    download_file(file_id, api_key, file_path)
-
-    extracted_text = extract_text_from_pdf(file_path)
+    if not file_id or not api_key:
+        print("Les variables d'environnement FILE_ID et CLOUD_API_KEY n'ont pas été définies.")
+        print("Le fichier par défaut sera un conte sur 'Le Corbeau et le Renard'.")
+        try:
+            extracted_text = extract_text_from_pdf(file_path)
+        except Exception as e:
+            print(f"Erreur lors de l'extraction du texte : {e}")
+            return
+    else :
+        try:
+            download_file(file_id, api_key, file_path)
+            extracted_text = extract_text_from_pdf(file_path)
+        except Exception as e:
+            print(f"Erreur lors du téléchargement ou de l'extraction du fichier : {e}")
+            return
     
-    chat = OllamaChat(system_prompt='Tu es un assistant intelligent. Tu réponds aux questions en fonction du contexte fourni de manière courte et concise.')
+    temperature = float(os.getenv("TEMPERATURE", 0.2))
+    chat = OllamaChat(system_prompt='Tu es un assistant intelligent. Tu réponds aux questions en fonction du contexte fourni de manière courte et concise.', temperature=temperature)
     chat.add_user_message(f"Voici le contenu du document que vous devez analyser : {extracted_text}")
     chat.interactive_chat()
 
